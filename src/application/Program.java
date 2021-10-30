13d117b6fcd267fcaa075a12da0fd5762f34df91
Pedro Fuziwara <fuziwarapedro@gmail.com>
2021-10-30 11:16:05 -0300

    Final 1.0

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+//made by Nelio Alves
 
 public class Program {
     public static void main(String[] args) {
